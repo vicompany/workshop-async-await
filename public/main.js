5c397f306a55e00d30f1af81fe0d1418b8f42a95
ba55ie <ba55ie@users.noreply.github.com>
2017-09-22 15:39:25 +0200

    Add xhr caching

--- public/main.js
+++ public/main.js
@@ -1,6 +1,9 @@
-import { error, org } from './templates.js';
+import { errorTpl, orgTpl, reposTpl } from './templates.js';
 
-const render = (el, data, tpl = error, append = false) => {
+const API_URL = 'https://api.github.com';
+const CACHE_TIMEOUT = 30 * 60 * 1000; // 30 minutes
+
+const render = (el, data, tpl = errorTpl, append = false) => {
 	const html = tpl(data);
 
 	if (append) {
@@ -10,20 +13,56 @@ const render = (el, data, tpl = error, append = false) => {
 	return (el.innerHTML = html);
 };
 
-// repos
-// - repo details
-// -- contributers
-// -- contributer counts
+// Use some client-side caching, otherwise Githubs ratelimiter will kick our asses.
+const cache = {
+	get(url) {
+		const item = JSON.parse(localStorage.getItem(url) || false);
 
-const BASE_URL = 'https://api.github.com';
+		if (item && item.timestamp) {
+			if (Date.now() - item.timestamp > CACHE_TIMEOUT) {
+				return item.response;
+			}
+
+			// Outdated
+			cache.remove(url);
+		}
+
+		return item;
+	},
+
+	set(url, response) {
+		const data = {
+			timestamp: Date.now(),
+			response,
+		};
+
+		try {
+			localStorage.setItem(url, JSON.stringify(data));
+		} catch (err) {
+			console.error(err);
+		}
+	},
+
+	remove(url) {
+		localStorage.removeItem(url);
+	},
+};
 
 const getJSON = (url, callback) => {
+	const fromCache = cache.get(url);
+
+	if (fromCache) {
+		return callback(null, fromCache);
+	}
+
 	const xhr = new XMLHttpRequest();
 
 	xhr.open('GET', url);
 
 	xhr.onload = () => {
 		if (xhr.status === 200) {
+			cache.set(url, xhr.responseText);
+
 			return callback(null, JSON.parse(xhr.responseText));
 		}
 
@@ -40,71 +79,29 @@ const getJSON = (url, callback) => {
 	}
 };
 
-const renderError = (el, msg = 'Oops!') => (el.textContent = msg);
-
-const renderUsers = (el, users) => {
-	const html = `
-		<dl>
-			${users.map(u => `
-				<div>
-					<dt>User</dt>
-					<dd><a href="/users/${u.id}"><img class="avatar" src="${u.avatar}" alt="${u.name}" />${u.name}</a></dd>
-					<dt>Transactions</dt>
-					<dd><a href="/users/${u.id}/transactions" class="js-load-transactions">Load</a></dd>
-				</div>
-			`).join('')}
-		</dl>
-	`;
-
-	el.innerHTML = html;
-};
-
-const renderTransactions = (el, transactions) => {
-	const html = `
-		<dl class="js-transactionlist">
-			${transactions.map(t => `
-				<dt><a href="/transactions/${t.id}">Transaction: ${t.id}</a></dt>
-				<dd><a href="/products/${t.productId}">Product: ${t.productId}</a><dd>
-				<dd class="todo">Product price/details<dd>
-			`).join('')}
-			<dt>Total $</dt>
-			<dd class="todo">transaction amount</dd>
-		</dl>
-	`;
-
-	el.innerHTML = html;
-};
-
-const loadTransactions = (el) => {
-	const { href, parentElement: container } = el;
-
-	if (!href) {
-		return;
-	}
-
-	getJSON(href, (err, transactions) => {
-		if (err) {
-			return renderError(container, 'Transaction unavailable!');
-		}
-
-		return renderTransactions(container, transactions);
-	});
-};
-
 // IIFE to kick it all off
 (() => {
-	const main = document.querySelector('main');
+	const main = document.querySelector('#org');
 
-	getJSON(`${BASE_URL}/orgs/vicompany`, (err, data) => {
+	getJSON(`${API_URL}/orgs/vicompany`, (err, org) => {
 		if (err) {
-			return render(main, 'Cannot retrieve org', error);
+			return render(main, err);
 		}
 
-		console.log('org', org(data), data);
+		render(main, org, orgTpl);
 
-		return render(main, data, org);
+		const { repos_url: reposUrl } = org;
+		const reposEl = document.querySelector('#repos');
 
-		// container.insertAdjacentHTML('beforeend', org(data));
+		getJSON(reposUrl, (err, repos) => {
+			if (err) {
+				render(reposEl, { message: 'They took my repos. Dook err derr!' });
+			}
+
+			console.log('repos', repos);
+
+			render(reposEl, repos, reposTpl);
+		});
 	});
 
 	// getJSON('/users', (err, users) => {
